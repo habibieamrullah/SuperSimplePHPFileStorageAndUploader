93fed82a185fbb02215a8c21a5f265ae7bcec5af
Habibie <habibieamrullah@gmail.com>
2020-11-04 09:44:11 +0700

    better

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $password = "admin";
     <head>
         
         <title>PHP File Storage</title>
-        <link rel="shortcut icon" href="https://dev.festivo.co/amd/favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="<?php echo $baseurl ?>favicon.ico" type="image/x-icon">
         <meta charset="utf-8">
         <meta http-equiv="Pragma" content="no-cache" />
         <meta http-equiv="Expires" content="0" />
@@ -53,7 +53,6 @@ $password = "admin";
             body{
                 
                 margin: 0;
-                padding: 20px;
                 font-family: 'Dosis', sans-serif;
             }
             
@@ -127,6 +126,26 @@ $password = "admin";
             .uploadform:hover{
                 border: 2px solid lime;
             }
+            
+            #topribbon{
+                position: -webkit-sticky;
+                position: sticky;
+                top: 0;
+                background-color: black;
+            }
+            
+            .tritem{
+                display: inline-block;
+                padding: 10px;
+            }
+            
+            .tritem:hover{
+                background-color: #171717;
+            }
+            
+            .contentwrapper{
+                padding: 10px;
+            }
         </style>
         
     </head>
@@ -138,11 +157,15 @@ $password = "admin";
                 $_SESSION["username"] = $username;
                 $_SESSION["password"] = $password;
                 ?>
-                <p>Login success!</p>
+                <div style="padding: 30px;">
+                    <p>Login success!</p>
+                </div>
                 <?php
             }else{
                 ?>
-                <p>Login failed</p>
+                <div style="padding: 30px;">
+                    <p>Login failed</p>
+                </div>
                 <?php
             }
             ?>
@@ -157,60 +180,91 @@ $password = "admin";
             if(isset($_SESSION["username"]) && isset($_SESSION["password"])){
                 
                 ?>
-                <h1><a href="index.php"><i class="fa fa-archive"></i> PHP File Storage</a></h1>
-                <?php
                 
-                if(isset($_POST["submitfile"])){
-                    $target_dir = "uploads/";
-                    $target_file = $target_dir . basename($_FILES["newfile"]["name"]);
-                    move_uploaded_file($_FILES["newfile"]["tmp_name"], $target_file);
-                }
+                <div id="topribbon">
+                    <a href="admin.php"><div class="tritem"><i class="fa fa-home"></i> Home</div></a>
+                    <a href="admin.php?filestorage"><div class="tritem"><i class="fa fa-archive"></i> File Storage</div></a>
+                    <a href="admin.php?logout"><div class="tritem"><i class="fa fa-sign-out"></i> Sign Out</div></a>
+                </div>
                 
-                if(isset($_GET["delete"])){
-					if(file_exists("uploads/" . $_GET["delete"])){
-						unlink("uploads/" . $_GET["delete"]);
-						echo "<div class='alert'>File is deleted successfully.</div>";
-					}
-				}
-                
-                
-                $dirpath = "uploads/*";
-				$files = array();
-				$files = glob($dirpath);
-				usort($files, function($x, $y) {
-					return filemtime($x) < filemtime($y);
-				});
-				
-				echo "<div>";
-				
-				foreach($files as $item){
-					echo "<div class='filethumb'>";
-					//echo "<div>" . $item . "</div>";
-					echo "<a href='" .$item. "' target='_blank'><div><i class='fa fa-file' style='font-size: 40px;'></i></div>";
-					echo "<div>" . str_replace("uploads/", "", $item) . "</div></a>";
-					echo "<a href='?delete=" .str_replace("uploads/", "", $item). "'><div style='color: red; margin-top: 20px; font-size: 10px;'><i class='fa fa-trash'></i> Delete</div></a>";
-					echo "</div>";
-				}
-				
-				
-				
-				if(count($files) == 0){
-    				?>
-                    <p>You have no file here. Try to begin uploading using the upload form at the bottom of this page.</p>
-                    <?php    
-				}
-                
-                echo "</div>";
-                
-                ?>
-                <div class="uploadform">
-                    <form method="post" enctype="multipart/form-data">
-    					<label><i class="fa fa-file"></i> Upload new file</label>
-    					<input class="fileinput" name="newfile" type="file">
-    					<input name = "submitfile" type="submit" value="Upload">
-    				</form>
+                <div class="contentwrapper">
+                    <?php
+                    
+                    if(isset($_GET["filestorage"])){
+                        ?>
+                        <h1>File Storage</h1>
+                        <?php
+                        if(isset($_POST["submitfile"])){
+                            $target_dir = "uploads/";
+                            $target_file = $target_dir . basename($_FILES["newfile"]["name"]);
+                            move_uploaded_file($_FILES["newfile"]["tmp_name"], $target_file);
+                        }
+                        
+                        if(isset($_GET["delete"])){
+        					if(file_exists("uploads/" . $_GET["delete"])){
+        						unlink("uploads/" . $_GET["delete"]);
+        						echo "<div class='alert'>File is deleted successfully.</div>";
+        					}
+        				}
+                        
+                        
+                        $dirpath = "uploads/*";
+        				$files = array();
+        				$files = glob($dirpath);
+        				usort($files, function($x, $y) {
+        					return filemtime($x) < filemtime($y);
+        				});
+        				
+        				echo "<div>";
+        				
+        				foreach($files as $item){
+        					echo "<div class='filethumb'>";
+        					//echo "<div>" . $item . "</div>";
+        					echo "<a href='" .$item. "' target='_blank'><div><i class='fa fa-file' style='font-size: 40px;'></i></div>";
+        					echo "<div>" . str_replace("uploads/", "", $item) . "</div></a>";
+        					echo "<a href='?delete=" .str_replace("uploads/", "", $item). "'><div style='color: red; margin-top: 20px; font-size: 10px;'><i class='fa fa-trash'></i> Delete</div></a>";
+        					echo "</div>";
+        				}
+        				
+        				
+        				
+        				if(count($files) == 0){
+            				?>
+                            <p>You have no file here. Try to begin uploading using the upload form at the bottom of this page.</p>
+                            <?php    
+        				}
+                        
+                        echo "</div>";
+                        
+                        ?>
+                        <div class="uploadform">
+                            <form method="post" enctype="multipart/form-data">
+            					<label><i class="fa fa-file"></i> Upload new file</label>
+            					<input class="fileinput" name="newfile" type="file">
+            					<input name = "submitfile" type="submit" value="Upload">
+            				</form>
+        				</div>
+        				<?php
+                    }else if(isset($_GET["logout"])){
+                        session_destroy();
+                        ?>
+                        <h1>Sign out</h1>
+                        <p>You are signed out. Good bye!</p>
+                        <script>
+                            setTimeout(function(){
+        				        location.href = "<?php echo $baseurl ?>admin.php";
+        				    }, 2500);
+                        </script>
+                        <?php
+                    }else{
+                        ?>
+                        <h1>Home</h1>
+                        <p>This is Dashboard home page.</p>
+                        <?php
+                    }
+                    
+                    ?>
 				</div>
-				
 				<script>
 				    setTimeout(function(){
 				        $(".alert").fadeOut();
@@ -220,7 +274,7 @@ $password = "admin";
                 
             }else{
                 ?>
-                <div align="left">
+                <div align="left" style="padding: 30px;">
                     <h1>Please login to access</h1>
                     <form method="post">
                         <div>
